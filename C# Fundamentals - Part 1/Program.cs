f8759ee31fbbfb5578e734c50e0ceb9a2fceeea6
alqrsan01 <hassanjamal428@gmail.com>
2026-07-01 22:57:32 +0400

    Solve task 9

--- C# Fundamentals - Part 1/Program.cs
+++ C# Fundamentals - Part 1/Program.cs
@@ -153,6 +153,38 @@
             Console.WriteLine("Original Bill Amount: " + billAmount);
             Console.WriteLine("Discount: " + discount);
             Console.WriteLine("Final Bill Amount: " + finalAmount);
+
+            // Task 9
+            Console.Write("Enter number form 1 to 7: ");
+            int day = int.Parse(Console.ReadLine());
+
+            switch (day)
+            {
+                case 1:
+                    Console.WriteLine("Sunday");
+                    break;
+                case 2:
+                    Console.WriteLine("Monday");
+                    break;
+                case 3:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 5:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 6:
+                    Console.WriteLine("Friday");
+                    break;
+                case 7:
+                    Console.WriteLine("Saturday");
+                    break;
+                default:
+                    Console.WriteLine("Invalid day number");
+                    break;
+            }
         }
     }
 }

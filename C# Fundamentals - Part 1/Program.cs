913bdd7b902179f6e82ccedc914f06574ce10999
alqrsan01 <hassanjamal428@gmail.com>
2026-07-01 21:22:20 +0400

    solve Task 1

---
+++ C# Fundamentals - Part 1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace C__Fundamentals___Part_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Task 1
+            string name = "Hassan";
+            int age = 25;
+            double height = 182.0;
+            bool student = true;
+
+            Console.WriteLine("Name: " + name + ", Age: " + age + ", Height: " + height + ", Student: " + student);
+        }
+    }
+}

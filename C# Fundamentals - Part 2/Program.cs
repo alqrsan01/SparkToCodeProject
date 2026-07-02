aef26e731d2614d63430fb846578a5d592921f89
alqrsan01 <hassanjamal428@gmail.com>
2026-07-02 23:57:30 +0400

    add project file + solve task 1

---
+++ C# Fundamentals - Part 2/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace C__Fundamentals___Part_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Task 1: Countdown Timer
+            Console.Write("Enter a starting number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = number; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Liftoff!");
+        }
+    }
+}

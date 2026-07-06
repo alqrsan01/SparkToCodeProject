5d385e83727e68eb590ceb2cc733c75c32dd7568
alqrsan01 <hassanjamal428@gmail.com>
2026-07-07 01:31:22 +0400

    Solve task 1

--- C# Fundamentals - Part 4/Program.cs
+++ C# Fundamentals - Part 4/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace C__Fundamentals___Part_4
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace C__Fundamentals___Part_4
 {
     internal class Program
     {
+        static void PrintWelcome(string user)
+        {
+            Console.WriteLine($"Hello, {user}");
+        }
         static void Main(string[] args)
         {
-            
+            // Task 1: Personalized Welcome Function
+            Console.Write("Enter your name: ");
+            string userName = Console.ReadLine();
+            PrintWelcome(userName);
+
+
         }
     }
 }

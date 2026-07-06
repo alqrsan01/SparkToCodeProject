77dc0e2a16ce090840f102b05d3aebb66e7ada1e
alqrsan01 <hassanjamal428@gmail.com>
2026-07-07 01:36:30 +0400

    Solve task 2

--- C# Fundamentals - Part 4/Program.cs
+++ C# Fundamentals - Part 4/Program.cs
@@ -4,9 +4,17 @@ namespace C__Fundamentals___Part_4
 {
     internal class Program
     {
+        // Task 1: Personalized Welcome Function
         static void PrintWelcome(string user)
         {
             Console.WriteLine($"Hello, {user}");
+        }
+
+
+        // Task 2: Square Number Function
+        static int Square(int x)
+        {
+            return x * x;
         }
         static void Main(string[] args)
         {
@@ -16,6 +24,11 @@ namespace C__Fundamentals___Part_4
             PrintWelcome(userName);
 
 
+            // Task 2: Square Number Function
+            Console.Write("Enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"The square of {number} is {Square(number)}");
         }
     }
 }

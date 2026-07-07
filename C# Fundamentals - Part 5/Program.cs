c70b712117920e5ce30a80f5a3f444c1872a43ee
alqrsan01 <hassanjamal428@gmail.com>
2026-07-07 20:29:52 +0400

    Solve task 1

---
+++ C# Fundamentals - Part 5/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace C__Fundamentals___Part_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Task 1: Fixed Grades Array
+            int[] grades = new int[5];
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write($"Enter grade {i + 1}: ");
+                int grade = int.Parse(Console.ReadLine());
+                grades[i] = grade;
+            }
+            foreach (int grade in grades)
+            {
+                Console.WriteLine($"Grade: {grade}");
+            }
+        }
+    }
+}

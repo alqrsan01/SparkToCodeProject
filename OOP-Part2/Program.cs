e619e8c42df80c734e91029f262117b36a0d9f2d
alqrsan01 <hassanjamal428@gmail.com>
2026-07-14 11:18:02 +0400

    declare system lists

--- OOP-Part2/Program.cs
+++ OOP-Part2/Program.cs
@@ -2,12 +2,19 @@
 {
     public class Room
     {
-        public int roomNumber;
-        public string roomType;
-        public double pricePerNight;
-        public bool isAvailable;
+        public int RoomNumber;
+        public string RoomType;
+        public double PricePerNight;
+        public bool IsAvailable;
 
-        public void displayRoom()
+        public Room(int roomNumber, string roomType, double pricePerNight)
+        {
+            RoomNumber = roomNumber;
+            RoomType = roomType;
+            PricePerNight = pricePerNight;
+            IsAvailable = true;
+        }
+        public void DisplayRoom()
         {
 
         }
@@ -15,17 +22,21 @@
 
     public class Guest
     {
-        public int guestId;
-        public string guestName;
-        public int roomNumber;
-        public DateTime checkIndate;
-        public int totalNights;
+        public int GuestId;
+        public string GuestName;
+        public int RoomNumber;
+        public string CheckIndate;
+        public int TotalNights;
 
-        public void displayGuest()
+        public Guest(string guestId, string guestName, string checkInDate, int totalNights)
         {
 
         }
-        public void calculateTotalCost()
+        public void DisplayGuest()
+        {
+
+        }
+        public void CalculateTotalCost()
         {
 
         }
@@ -34,7 +45,16 @@
     {
         static void Main(string[] args)
         {
-            
+            List<Room> rooms = new List<Room>();
+            List<Guest> guests = new List<Guest>();
+
+            rooms.Add(new Room(101, "Single", 100.0));
+            rooms.Add(new Room(102, "Double", 150.0));
+            rooms.Add(new Room(103, "Suite", 250.0));
+            rooms.Add(new Room(104, "Single", 100.0));
+            rooms.Add(new Room(105, "Double", 150.0));
+            rooms.Add(new Room(106, "Suite", 250.0));
+            rooms.Add(new Room(107, "Single", 100.0));
         }
     }
 }

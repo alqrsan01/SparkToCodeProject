cbbb33c53ea2530b9fa7766b8511630fbefc480b
alqrsan01 <hassanjamal428@gmail.com>
2026-07-14 11:04:53 +0400

    Create classes

--- OOP-Part2/Program.cs
+++ OOP-Part2/Program.cs
@@ -1,8 +1,34 @@
 ﻿namespace OOP_Part2
 {
-    class Room
+    public class Room
     {
+        public int roomNumber;
+        public string roomType;
+        public double pricePerNight;
+        public bool isAvailable;
 
+        public void displayRoom()
+        {
+
+        }
+    }
+
+    public class Guest
+    {
+        public int guestId;
+        public string guestName;
+        public int roomNumber;
+        public DateTime checkIndate;
+        public int totalNights;
+
+        public void displayGuest()
+        {
+
+        }
+        public void calculateTotalCost()
+        {
+
+        }
     }
     public class Program
     {

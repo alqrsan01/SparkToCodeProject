964e6472ec7d35ca5f097fe430961687f55d80de
alqrsan01 <hassanjamal428@gmail.com>
2026-07-25 22:34:28 +0400

    register all models

--- EFCoreProject/ProjectContext.cs
+++ EFCoreProject/ProjectContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using EFCoreProject.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,6 +8,11 @@ namespace EFCoreProject
 {
     public class ProjectContext : DbContext
     {
-
+        DbSet<Employee> employees;
+        DbSet<Department> departments;
+        DbSet<DepartmentLocation> departmentLocations;
+        DbSet<Project> projects;
+        DbSet<WorksOn> worksOns;
+        DbSet<Dependent> dependents;
     }
 }

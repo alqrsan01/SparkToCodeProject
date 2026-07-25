d66e895b0a2c1486ece375840aaea0f7491c2593
alqrsan01 <hassanjamal428@gmail.com>
2026-07-25 23:19:01 +0400

    add data manually

--- EFCoreProject/Program.cs
+++ EFCoreProject/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace EFCoreProject
+﻿using EFCoreProject.Models;
+
+namespace EFCoreProject
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ProjectContext context = new ProjectContext();
+
+            // add data on table employee
+            Employee e1 = new Employee();
+            e1.Fname = "Hassan";
+            e1.Minit = "A";
+            e1.Lname = "Ambusaidi";
+            e1.Ssn = 123456789;
+            e1.Bdate = "2001-03-20";
+            e1.Address = "Nizwa";
+            e1.Sex = 'M';
+            e1.Salary = 1000;
+            e1.Super_ssn = 123456789;
+            e1.DepartmentNumber = 1;
+            
+            context.employees.Add(e1);
+            context.SaveChanges();
+
         }
     }
 }

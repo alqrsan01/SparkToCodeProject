c53b8716d9ab6d306b0493e63fe3679ab1eba2c9
alqrsan01 <hassanjamal428@gmail.com>
2026-07-27 10:30:36 +0400

    relationship

--- EFCoreProject/Models/Department.cs
+++ EFCoreProject/Models/Department.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace EFCoreProject.Models
@@ -12,6 +13,18 @@ namespace EFCoreProject.Models
         public int DepartmentNumber { get; set; }
         public string DepartmentName { get; set; }
         public int ManagerSsn { get; set; }
+
+        // work on relation
+        [InverseProperty("D")]
+        public List<Employee> Employees { get; set; }
+
+        // manage relation
+        [ForeignKey("Employee")]
+        public int EmoloyeeId { get; set; }
+
+        public Employee Employee { get; set; }
+
+        // location relation
         public string ManagerStartDate { get; set; }
     }
 }

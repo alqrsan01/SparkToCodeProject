d155e628b9337ab5ba069bd033219a05f465e5fb
alqrsan01 <hassanjamal428@gmail.com>
2026-07-30 00:23:18 +0400

    create controllers

--- WebAPIProject/Controllers/CategoryController.cs
+++ WebAPIProject/Controllers/CategoryController.cs
@@ -1,6 +1,33 @@
-﻿namespace WebAPIProject.Controllers
+﻿using WebAPIProject.Models;
+
+namespace WebAPIProject.Controllers
 {
     public class CategoryController
     {
+        private ProjectContext context;
+        public CategoryController(ProjectContext _context)
+        {
+            context = _context;
+        }
+
+        public void AddCategory(Category p)
+        {
+            context.Categories.Add(p);
+            context.SaveChanges();
+        }
+
+        public void RemoveCategory(int id)
+        {
+            Category category = context.Categories.FirstOrDefault(c => c.CategoryId == id);
+            if (category == null)
+            {
+
+            }
+            else
+            {
+                context.Categories.Remove(category);
+                context.SaveChanges();
+            }
+        }
     }
 }
--- WebAPIProject/Controllers/ProductController.cs
+++ WebAPIProject/Controllers/ProductController.cs
@@ -1,6 +1,33 @@
-﻿namespace WebAPIProject.Controllers
+﻿using WebAPIProject.Models;
+
+namespace WebAPIProject.Controllers
 {
     public class ProductController
     {
+        private ProjectContext context;
+        public ProductController(ProjectContext _context)
+        {
+            context = _context;
+        }
+
+        public void AddProduct(Product p)
+        {
+            context.Products.Add(p);
+            context.SaveChanges();
+        }
+
+        public void RemoveProduct(int id)
+        {
+            Product product = context.Products.FirstOrDefault(p => p.ProductId == id);
+            if (product == null)
+            {
+                
+            }
+            else
+            {
+                context.Products.Remove(product);
+                context.SaveChanges();
+            }
+        }
     }
 }
